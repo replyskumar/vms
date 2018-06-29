035a95fd2c5369c8358a525a7b29a0202565dab3
Rajeev R Menon <rajeevrmenon97@gmail.com>
2018-06-29 11:05:27 +0530

    django results backend added

--- vms/celery.py
+++ vms/celery.py
@@ -1,13 +1,19 @@
 from __future__ import absolute_import, unicode_literals
 from celery import Celery
 import os
+from vms.settings import USE_ELASTIC_SEARCH
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vms.settings')
 
 app = Celery('vms', broker=os.environ.get('REDIS_URL','redis://localhost'), include=['cve.tasks','cpe.tasks'])
 
+if USE_ELASTIC_SEARCH:
+    backend_url = os.environ.get('BONSAI_URL','https://localhost:9200').replace('https','elasticsearch')+'/celery/results'
+else:
+    backend_url = 'django-db'
+
 app.conf.update(
-    result_backend=os.environ.get('BONSAI_URL','https://localhost:9200').replace('https','elasticsearch')+'/celery/results',
+    result_backend=backend_url,
     result_expires=86400*2,
     timezone = 'Asia/Kolkata'
 )
